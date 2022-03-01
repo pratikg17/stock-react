0fabc4064cce8818a5469449c562483c4fcad726
Pratik Gawand <gawand.p@northeastern.edu>
2022-02-28 21:34:09 -0500

    Updates and ticker

--- src/pages/investor/Home.js
+++ src/pages/investor/Home.js
@@ -5,9 +5,8 @@ import { Button, Row, Col, Space, Tag } from "antd";
 import Spinner from "../../components/Spinner";
 import { Link } from "react-router-dom";
 import { getAllStocks } from "../../redux/actions/stocksActions";
-// import io from "socket.io-client";
+
 import useWebSocket, { ReadyState } from "react-use-websocket";
-import Portfolio from "./Portfolio";
 
 function Home() {
   const { loading } = useSelector((state) => state.alertsReducer);
@@ -50,21 +49,46 @@ function Home() {
   const getMarketCap = () => {
     if (stocks.length != 0) {
       console.log(stocks);
-
+      let ticker = [];
       let marketCap = 0;
       let totalVol = 0;
       for (let i = 0; i < stocks.length; i++) {
         let s = stocks[i];
         marketCap += parseInt(s.volume) * parseFloat(s.currentPrice);
         totalVol += parseInt(s.volume);
+
+        let colorTag = "green";
+        let iconTag = "fa-solid fa-arrow-trend-up";
+
+        let percentChange =
+          ((parseFloat(s.currentPrice) - parseFloat(s.openPrice)) /
+            parseFloat(s.currentPrice)) *
+          100;
+        if (percentChange < 0) {
+          colorTag = "red";
+          iconTag = "fa-solid fa-arrow-trend-down";
+        }
+
+        ticker.push({
+          ...s,
+          percentChange: percentChange,
+          colorTag,
+          iconTag,
+        });
       }
 
       return (
         <Space className="mb-2" size="middle">
-          <Tag color="green">
-            {" "}
-            Market Capitalization ${marketCap.toFixed(2)}
-          </Tag>
+          {ticker.map((tick) => {
+            console.log(tick);
+            return (
+              <Tag color={tick.colorTag}>
+                {tick.stockName} {tick.tickerName}{" "}
+                {tick.percentChange.toFixed(2)}% <i class={tick.iconTag}></i>
+              </Tag>
+            );
+          })}
+          <Tag color="green">Market Capitalization ${marketCap.toFixed(2)}</Tag>
           <Tag color="green"> Market Volume #{totalVol}</Tag>
         </Space>
       );
@@ -75,165 +99,87 @@ function Home() {
   return (
     <DefaultLayout>
       {loading == true && <Spinner />}
-      {/* <Row justify="center" gutter={16} className="mt-3">
-        <div className="d-flex justify-content-between align-items-center">
-          <h3 className="mt-1 mr-3">Dashboard</h3>
-        </div>
-      </Row> */}
 
       <Row gutter={[16, 8]} className="mt-3">
         <marquee> {getMarketCap()} </marquee>
-        <Col lg={12}>
+        <Col lg={24}>
           <h4>Stocks</h4>
 
           <div className="mt-1 mr-3 d-flex justify-content-between flex-row flex-wrap">
             {stocks.map((stock) => {
               return (
                 <Col lg={12} className="mb-2">
-                  <div className="car p-2 bs1">
-                    <div className=" d-flex align-items-center justify-content-between">
+                  <div className="p-2 bs1">
+                    <div className=" d-flex align-items-center justify-content-around">
                       <div className="text-left pl-2">
-                        <p>
-                          <Space size="middle">
-                            <b>{stock.tickerName}</b>
-                            <Tag color="orange">{stock.stockName}</Tag>
-                          </Space>
-                        </p>
-                        <p></p>
-                        <p>
-                          <Space size="middle">
-                            <div>Live Price</div>
-                            <Tag color="green">
-                              {"$" + parseFloat(stock.currentPrice).toFixed(2)}
-                            </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            <div>Daily High</div>
+                        <div className="d-flex justify-content-between align-items-center align-content-center ">
+                          <h5>{stock.stockName}</h5>
+                          <Tag color="orange">{stock.tickerName}</Tag>
+                        </div>
+                        <div className="d-flex justify-content-between align-items-center align-content-center ">
+                          <h6>Live Price</h6>
+                          <Tag color="green">
+                            {"$" + parseFloat(stock.currentPrice).toFixed(2)}
+                          </Tag>
+                        </div>
+                        {/* <div className="d-flex justify-content-between align-items-center align-content-center ">
+                          <h6>Daily High</h6>
+                          <Tag color="green">
+                            {"$" + parseFloat(stock.dailyHigh).toFixed(2)}
+                          </Tag>
+                        </div>
+                        <div className="d-flex justify-content-between align-items-center align-content-center ">
+                          <h6>Daily Low</h6>
+                          <Tag color="red">
+                            {"$" + parseFloat(stock.dailyLow).toFixed(2)}
+                          </Tag>
+                        </div> */}
+
+                        <div className="d-flex justify-content-between align-items-center align-content-center ">
+                          <h6>Volume</h6>
+                          <Tag color="blue">
+                            {parseFloat(stock.volume).toFixed(0)}
+                          </Tag>
+                        </div>
+
+                        <div className="d-flex justify-content-between align-items-center align-content-center ">
+                          <div>
+                            Daily High &nbsp;
                             <Tag color="green">
                               {"$" + parseFloat(stock.dailyHigh).toFixed(2)}
                             </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            Daily Low
+                          </div>
+                          <div>
+                            Daily Low &nbsp;
                             <Tag color="red">
                               {"$" + parseFloat(stock.dailyLow).toFixed(2)}
                             </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            Volume
-                            <Tag color="blue">
-                              {parseFloat(stock.volume).toFixed(0)}
-                            </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            <div>
-                              OPEN &nbsp;
-                              <Tag color="blue">
-                                {"$" + parseFloat(stock.openPrice).toFixed(2)}
-                              </Tag>
-                            </div>
-                            <div>
-                              CLOSE &nbsp;
-                              <Tag color="blue">
-                                {"$" + parseFloat(stock.closePrice).toFixed(2)}
-                              </Tag>
-                            </div>
-                          </Space>
-                        </p>
+                          </div>
+                        </div>
                       </div>
 
-                      <div>
-                        <button className="btn1-green mr-2">
+                      <div className="d-flex justify-content-around flex-column">
+                        <button className="btn1-green mt-2">
                           {/* <Link to={`/booking/${car._id}`}>Book Now</Link> */}
-                          <Link>BUY</Link>
+                          <Link to={`/add-buy-orders/`}>BUY</Link>
                         </button>
-                      </div>
-                    </div>
-                  </div>
-                </Col>
-              );
-            })}
-          </div>
-        </Col>
-        <Col lg={12}>
-          <h4>Portfolio</h4>
-          <div className="mt-1 mr-3 d-flex justify-content-between flex-row flex-wrap">
-            {stocks.map((stock) => {
-              return (
-                <Col lg={12} className="mb-2">
-                  <div className="car p-2 bs1">
-                    <div className=" d-flex align-items-center justify-content-between">
-                      <div className="text-left pl-2">
-                        <p>
-                          <Space size="middle">
-                            <b>{stock.tickerName}</b>
-                            <Tag color="orange">{stock.stockName}</Tag>
-                          </Space>
-                        </p>
-                        <p></p>
-                        <p>
-                          <Space size="middle">
-                            <div>Live Price</div>
-                            <Tag color="green">
-                              {"$" + parseFloat(stock.currentPrice).toFixed(2)}
-                            </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            <div>Daily High</div>
-                            <Tag color="green">
-                              {"$" + parseFloat(stock.dailyHigh).toFixed(2)}
-                            </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            Daily Low
-                            <Tag color="red">
-                              {"$" + parseFloat(stock.dailyLow).toFixed(2)}
-                            </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            Volume
+                        <button className="btn1-red mt-2 ">
+                          <Link to={`/add-sell-orders/`}>SELL</Link>
+                        </button>
+                        <div className="d-flex justify-content-between align-items-center align-content-center mt-3">
+                          <div>
+                            OPEN &nbsp;
                             <Tag color="blue">
-                              {parseFloat(stock.volume).toFixed(0)}
+                              {"$" + parseFloat(stock.openPrice).toFixed(2)}
                             </Tag>
-                          </Space>
-                        </p>
-                        <p>
-                          <Space size="middle">
-                            <div>
-                              OPEN &nbsp;
-                              <Tag color="blue">
-                                {"$" + parseFloat(stock.openPrice).toFixed(2)}
-                              </Tag>
-                            </div>
-                            <div>
-                              CLOSE &nbsp;
-                              <Tag color="blue">
-                                {"$" + parseFloat(stock.closePrice).toFixed(2)}
-                              </Tag>
-                            </div>
-                          </Space>
-                        </p>
-                      </div>
-
-                      <div>
-                        <button className="btn1-green mr-2">
-                          {/* <Link to={`/booking/${car._id}`}>Book Now</Link> */}
-                          <Link>BUY</Link>
-                        </button>
+                          </div>
+                          <div>
+                            CLOSE &nbsp;
+                            <Tag color="blue">
+                              {"$" + parseFloat(stock.closePrice).toFixed(2)}
+                            </Tag>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
